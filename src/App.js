f0616c18c10e11fb6b9ca6f32bd2e54753eb8c5b
Joseph Dickens <josephdickens87@gmail.com>
2022-01-08 19:21:32 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -11,13 +11,16 @@ function App() {
   let timerId = useRef();
 
   const [tableData, setTableData] = useState({
+    firstPlace: 0,
+    secondPlace: 0,
+    thirdPlace: 0,
     smallBlind: 0,
     bigBlind: 0,
     ante: 0,
     buttonPosition: 0,
   });
 
-  const [playerData, setPlayerData] = useState([123, 23423, 2342, 1231, 12313, 2131, 12]);
+  const [playerData, setPlayerData] = useState([]);
   const [gameType, setGameType] = useState("ignitionNineMan");
 
   const stacksList = () => {
@@ -98,15 +101,50 @@ function App() {
         stacks.length
       );
 
-      return [...afterBlinds, ...beforeBlinds];
+      solve([...afterBlinds, ...beforeBlinds]);
     } else {
-      const beforeBlinds = stacks.splice(0, tableData.buttonPosition + 2);
-      return [...beforeBlinds, ...stacks];
+      const newStacks = [];
+      let blindStartIndex = tableData.buttonPosition - 1;
+
+      const buttonAndBlinds = stacks.splice(tableData.buttonPosition -1, 3);
+      
+      buttonAndBlinds.forEach((blind, index) => {
+        newStacks.push({
+          index: blindStartIndex,
+          stack: blind
+        })
+        blindStartIndex += 1
+      })
+
+      while(newStacks.length < 3) {
+        newStacks.push({
+          index: blindStartIndex,
+          stack: stacks.shift()
+        })
+        blindStartIndex += 1
+      }
+
+      stacks.forEach((stack, index) => {
+        newStacks.push({
+          index,
+          stack
+        })
+      })
+      newStacks.sort((a, b) => a.index - b.index)
+      solve(Array.from(newStacks.keys()).map(key => newStacks[key].stack))
     }
   };
 
   const solve = (stacks) => {
-    console.log(stacks)
+    let apiString = "https://www.holdemresources.net/nashicm?action=calculate&";
+
+    stacks.forEach((stack, index) => {
+      apiString += `s${index + 1}=${stack}&`;
+    });
+
+    apiString += `p1=${tableData.firstPlace}&p2=${tableData.secondPlace}&p3=${tableData.thirdPlace}&sb=${tableData.smallBlind}&bb=${tableData.bigBlind}&ante=${tableData.ante}`;
+
+    window.open(apiString, "_blank");
   } 
   return (
     <div className="container-fluid p-3">
@@ -140,6 +178,39 @@ function App() {
                   </button>
                 </div>
               </div>
+              <input
+                type="number"
+                className="form-control"
+                placeholder="1st"
+                onChange={(e) =>
+                  setTableData({
+                    ...tableData,
+                    firstPlace: parseInt(e.target.value),
+                  })
+                }
+              />
+              <input
+                type="number"
+                className="form-control"
+                placeholder="2nd"
+                onChange={(e) =>
+                  setTableData({
+                    ...tableData,
+                    secondPlace: parseInt(e.target.value),
+                  })
+                }
+              />
+               <input
+                type="number"
+                className="form-control"
+                placeholder="3rd"
+                onChange={(e) =>
+                  setTableData({
+                    ...tableData,
+                    thirdPlace: parseInt(e.target.value),
+                  })
+                }
+              />
               <input
                 type="number"
                 className="form-control"
